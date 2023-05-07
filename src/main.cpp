040ddfa7041127f47360dcd4337083890794ed27
1Hyena <hyena@hyena.net.ee>
2023-05-08 02:55:04 +0300

    Parsing the HTML, work in progress.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <limits>
 #include <md4c-html.h>
+#include <tidy.h>
+#include <tidybuffio.h>
 
 void process_output(const MD_CHAR *, MD_SIZE, void *);
 
@@ -56,6 +58,111 @@ int main(int argc, char **argv) {
         md_text.c_str(), MD_SIZE(md_text.size()), process_output,
         &html_text, MD_DIALECT_GITHUB, 0
     );
+
+    {
+        TidyBuffer tidy_buffer{};
+        TidyDoc tdoc = tidyCreate();
+
+        tidyParseString( tdoc, html_text.c_str() );
+        html_text.clear();
+
+        /*
+        TidyNode parent = tidyGetRoot(tdoc);
+        int depth = 0;
+
+        while (parent) {
+            const char *name = tidyNodeGetName(parent);
+
+            if (name) {
+                std::cout << std::string(2*depth, ' ') << "[" << name << "]\n";
+            }
+
+            if (tidyNodeGetId(parent) == TidyTag_H1) {
+                std::cout << "BOOM\n";
+            }
+
+            TidyNode child = tidyGetChild(parent);
+
+            if (child) {
+                parent = child;
+                ++depth;
+                continue;
+            }
+
+            TidyNode sibling = tidyGetNext(parent);
+
+            if (sibling) {
+                parent = sibling;
+                continue;
+            }
+
+            do {
+                parent = tidyGetParent(parent);
+
+                if (!parent) break;
+
+                sibling = tidyGetNext(parent);
+
+                --depth;
+            }
+            while (!sibling);
+
+            parent = sibling;
+        }
+        */
+
+        TidyNode body = tidyGetBody(tdoc);
+
+        if (body) {
+            TidyNode sibling = tidyGetChild(body);
+
+            std::vector<std::string> tabs;
+
+            for (; sibling; sibling = tidyGetNext(sibling)) {
+                if (tidyNodeIsHeader(sibling)) {
+                    if (tidyNodeGetId(sibling) == TidyTag_H1) {
+                        tabs.emplace_back();
+                    }
+
+                    TidyNode child = tidyGetChild(sibling);
+
+                    if (tidyNodeHasText(tdoc, child)) {
+                        tidyBufClear(&tidy_buffer);
+                        tidyNodeGetValue(tdoc, child, &tidy_buffer);
+
+                        tabs.back().append("<!--").append(
+                            (const char *) tidy_buffer.bp, tidy_buffer.size
+                        ).append("-->\n");
+                    }
+
+                    continue;
+                }
+
+                if (tabs.empty()) continue;
+
+                tidyBufClear(&tidy_buffer);
+                tidyNodeGetText(tdoc, sibling, &tidy_buffer);
+
+                tabs.back().append(
+                    (const char *) tidy_buffer.bp, tidy_buffer.size
+                );
+            }
+
+            for (size_t i=0; i<tabs.size(); ++i) {
+                std::cout << "NEW TAB:\n" << tabs[i] << "\n";
+            }
+        }
+
+
+
+        //tidyCleanAndRepair( tdoc );
+        //tidySaveBuffer( tdoc, &tidy_buffer );
+
+        //html_text.assign((const char *) tidy_buffer.bp, tidy_buffer.size);
+
+        tidyRelease( tdoc );
+        tidyBufFree( &tidy_buffer );
+    }
 
     std::cout << html_text << "\n";
 
