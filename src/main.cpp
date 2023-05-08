91488119ba8c341219638632158944ad42066c7a
1Hyena <hyena@hyena.net.ee>
2023-05-09 01:40:08 +0300

    Work in progress.

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,58 @@
 #include <md4c-html.h>
 #include <tidy.h>
 #include <tidybuffio.h>
+#include <tinyxml2.h>
+#include <list>
+#include <signal.h>
 
 void process_output(const MD_CHAR *, MD_SIZE, void *);
+
+const tinyxml2::XMLElement *find_if(
+    const tinyxml2::XMLElement &root,
+    std::function<bool(const tinyxml2::XMLElement &, int)> fun
+) {
+    int depth = 0;
+
+    const tinyxml2::XMLElement *parent = &root;
+
+    while (parent) {
+        if (fun(*parent, depth)) {
+            return parent;
+        }
+
+        const tinyxml2::XMLElement *child = parent->FirstChildElement();
+
+        if (child) {
+            parent = child;
+            ++depth;
+            continue;
+        }
+
+        const tinyxml2::XMLElement *sibling = parent->NextSiblingElement();
+
+        if (sibling) {
+            parent = sibling;
+            continue;
+        }
+
+        do {
+            const tinyxml2::XMLNode *parent_node = parent->Parent();
+
+            parent = parent_node ? parent_node->ToElement() : nullptr;
+
+            if (!parent) break;
+
+            sibling = parent->NextSiblingElement();
+
+            --depth;
+        }
+        while (!sibling);
+
+        parent = sibling;
+    }
+
+    return nullptr;
+}
 
 int main(int argc, char **argv) {
     OPTIONS options("MarkDown Monolith Assembler", "1.0", "Erich Erstu");
@@ -35,7 +85,7 @@ int main(int argc, char **argv) {
         md_text.assign(std_input.str());
     }
     else {
-        std::ifstream input(options.file);
+        std::ifstream input(options.file, std::ios::binary);
 
         if (!input) {
             std::cerr << options.file << ": " << strerror(errno) << "\n";
@@ -52,129 +102,134 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    std::string html_text;
+    std::string xhtml_text;
 
     fail = md_html(
         md_text.c_str(), MD_SIZE(md_text.size()), process_output,
-        &html_text, MD_DIALECT_GITHUB, MD_HTML_FLAG_XHTML
+        &xhtml_text, MD_DIALECT_GITHUB, MD_HTML_FLAG_XHTML
     );
+
+    {
+        tinyxml2::XMLDocument doc;
+
+        doc.Parse(xhtml_text.c_str(), xhtml_text.size());
+
+        tinyxml2::XMLElement *parent = doc.RootElement();
+        const tinyxml2::XMLElement *first_heading = nullptr;
+
+        std::list<tinyxml2::XMLDocument> chapters;
+
+        tinyxml2::XMLPrinter printer;
+
+        if (parent) {
+            first_heading = find_if(
+                *parent,
+                [](const tinyxml2::XMLElement &el, int depth) {
+                    const char *name = el.Name();
+
+                    if (name) {
+                        if (!strcasecmp("h1", name)) {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+            );
+
+            const tinyxml2::XMLElement *sibling = first_heading;
+            int next_id=1;
+
+            for (; first_heading; sibling = sibling->NextSiblingElement()) {
+                const char *name = sibling ? sibling->Name() : nullptr;
+
+                tinyxml2::XMLElement *root = nullptr;
+
+                if ((name && !strcasecmp("h1", name)) || !sibling) {
+                    if (!chapters.empty()) {
+                        chapters.back().Print(&printer);
+                    }
+
+                    if (!sibling) {
+                        break;
+                    }
+
+                    chapters.emplace_back();
+
+                    tinyxml2::XMLNode *node{
+                        chapters.back().InsertFirstChild(
+                            chapters.back().NewElement("div")
+                        )
+                    };
+
+                    tinyxml2::XMLElement *elem{
+                        node ? node->ToElement() : nullptr
+                    };
+
+                    if (elem) {
+                        elem->SetAttribute("class", "tab");
+
+                        node = elem->InsertFirstChild(
+                            chapters.back().NewElement("a")
+                        );
+
+                        elem = node ? node->ToElement() : nullptr;
+
+                        if (elem) {
+                            std::string id{"chapter-"};
+                            id.append(std::to_string(next_id++));
+
+                            elem->SetAttribute("id", id.c_str());
+                            elem->SetAttribute(
+                                "href", std::string("#").append(id).c_str()
+                            );
+
+                            root = elem;
+                        }
+                        else raise(SIGSEGV);
+                    }
+                    else raise(SIGSEGV);
+                }
+                else {
+                    root = chapters.back().RootElement();
+                }
+
+                if (chapters.empty()) continue;
+
+                tinyxml2::XMLNode *node = sibling->DeepClone(&chapters.back());
+
+                if (node) {
+                    root->InsertEndChild(node);
+                }
+            }
+        }
+
+        xhtml_text.assign(printer.CStr());
+    }
 
     {
         TidyBuffer tidy_buffer{};
         TidyDoc tdoc = tidyCreate();
 
-        //tidyOptSetBool(tdoc, TidyXmlTags, yes);
-        //tidyOptSetBool(tdoc, TidyHtmlOut, yes);
-        //tidyOptSetBool(tdoc, TidyXmlTags, no);
-        //tidyOptSetBool(tdoc, TidyXmlOut, no);
-        tidyParseString( tdoc, html_text.c_str() );
-        html_text.clear();
+        tidyOptSetBool(tdoc, TidyIndentContent, yes);
+        tidyParseString( tdoc, xhtml_text.c_str() );
+        xhtml_text.clear();
 
-        //tidyCleanAndRepair( tdoc );
+        tidyCleanAndRepair( tdoc );
 
         tidySaveBuffer( tdoc, &tidy_buffer );
-        html_text.assign((const char *) tidy_buffer.bp, tidy_buffer.size);
+        xhtml_text.assign((const char *) tidy_buffer.bp, tidy_buffer.size);
 
         tidyRelease( tdoc );
         tidyBufFree( &tidy_buffer );
-
-        /*
-        TidyNode parent = tidyGetRoot(tdoc);
-        int depth = 0;
-
-        while (parent) {
-            const char *name = tidyNodeGetName(parent);
-
-            if (name) {
-                std::cout << std::string(2*depth, ' ') << "[" << name << "]\n";
-            }
-
-            if (tidyNodeGetId(parent) == TidyTag_H1) {
-                std::cout << "BOOM\n";
-            }
-
-            TidyNode child = tidyGetChild(parent);
-
-            if (child) {
-                parent = child;
-                ++depth;
-                continue;
-            }
-
-            TidyNode sibling = tidyGetNext(parent);
-
-            if (sibling) {
-                parent = sibling;
-                continue;
-            }
-
-            do {
-                parent = tidyGetParent(parent);
-
-                if (!parent) break;
-
-                sibling = tidyGetNext(parent);
-
-                --depth;
-            }
-            while (!sibling);
-
-            parent = sibling;
-        }
-        */
-
-        /*
-        TidyNode body = tidyGetBody(tdoc);
-
-        if (body) {
-            TidyNode sibling = tidyGetChild(body);
-
-            std::vector<std::string> tabs;
-
-            for (; sibling; sibling = tidyGetNext(sibling)) {
-                if (tidyNodeIsHeader(sibling)) {
-                    if (tidyNodeGetId(sibling) == TidyTag_H1) {
-                        tabs.emplace_back();
-                    }
-
-                    TidyNode child = tidyGetChild(sibling);
-
-                    if (tidyNodeHasText(tdoc, child)) {
-                        tidyBufClear(&tidy_buffer);
-                        tidyNodeGetValue(tdoc, child, &tidy_buffer);
-
-                        tabs.back().append("<!--").append(
-                            (const char *) tidy_buffer.bp, tidy_buffer.size
-                        ).append("-->\n");
-                    }
-
-                    continue;
-                }
-
-                if (tabs.empty()) continue;
-
-                tidyBufClear(&tidy_buffer);
-                tidyNodeGetText(tdoc, sibling, &tidy_buffer);
-
-                tabs.back().append(
-                    (const char *) tidy_buffer.bp, tidy_buffer.size
-                );
-            }
-
-            for (size_t i=0; i<tabs.size(); ++i) {
-                std::cout << "NEW TAB:\n" << tabs[i] << "\n";
-            }
-        }
-
-        */
     }
 
-    std::cout << html_text << "\n";
+    std::cout << xhtml_text << "\n";
 
     return fail ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 void process_output(const MD_CHAR *str, MD_SIZE len, void *userdata) {
-    std::string *html_text = static_cast<std::string *>(userdata);
-    html_text->append(str, len);
+    std::string *dest = static_cast<std::string *>(userdata);
+    dest->append(str, len);
 }
