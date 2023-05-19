0f1c20812547bd9dbffeb770cff51b9b259eade6
1Hyena <hyena@hyena.net.ee>
2023-05-19 17:42:58 +0300

    Changed screenshot edge style.

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./mdma README.md >README.html
 chromium --headless --screenshot=screenshot.jpg README.html
-magick screenshot.jpg -gamma 0.8 -raise 4 -raise 8 -raise 12 screenshot.jpg
+magick screenshot.jpg -gamma 0.8 -raise 1 -raise 2 -raise 3 screenshot.jpg
