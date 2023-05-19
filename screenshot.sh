994a6251b56ba17fb01cedb44d7a5223983fd330
1Hyena <hyena@hyena.net.ee>
2023-05-19 17:21:13 +0300

    Increased the feather radius of the screenshot.

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./mdma README.md >README.html
 chromium --headless --screenshot=screenshot.png README.html
-magick screenshot.png -gamma 0.8 -alpha set -virtual-pixel transparent -channel A -blur 0x8 -level 50%,100% +channel screenshot.png
+magick screenshot.png -gamma 0.8 -alpha set -virtual-pixel transparent -channel A -blur 0x16 -level 50%,100% +channel screenshot.png
