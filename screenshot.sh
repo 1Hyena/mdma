b6701a57ac55c91eb1737a5649b4bb00c4dd77d8
1Hyena <hyena@hyena.net.ee>
2023-06-01 23:38:52 +0300

    Now using grid instead of the empty span hack for vertically aligning content. Separated framework HTML refreshing functionality from the screenshot.sh script and moved it into its own script named compile.sh.

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-./mdma README.md >framework.html
 chromium --headless --screenshot=screenshot.jpg framework.html
 magick screenshot.jpg -gamma 0.8 -raise 1 -raise 2 -raise 3 screenshot.jpg
