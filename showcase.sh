82f588edf469360e2532da5c42562c85f760ddaa
1Hyena <hyena@hyena.net.ee>
2024-10-26 00:31:25 +0300

    Added new website to the showcase.

--- showcase.sh
+++ showcase.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd showcase
 chromium --headless --force-device-scale-factor=1 --window-size=1280,720 --screenshot=ericherstu.net.jpg https://ericherstu.net/
-mogrify -gamma 0.8 -raise 1 -raise 2 -raise 3 *.jpg
+chromium --headless --force-device-scale-factor=1 --window-size=1280,720 --screenshot=stonia.net.ee.jpg https://stonia.net.ee/
+mogrify -trim -gamma 0.8 -raise 1 -raise 2 -raise 3 *.jpg
